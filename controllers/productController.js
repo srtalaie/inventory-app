822dc8003e1bb4d69915d36d0daf5bff0e9d05fc
srtalaie <srt4721@yahoo.com>
2024-04-16 15:23:45 -0700

    Product create added

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,7 @@ const Category = require("../models/category")
 
 const asyncHandler = require("express-async-handler")
 const { body, validationResult } = require("express-validator")
+const { all } = require("../routes/products")
 
 // Product detail
 exports.product_detail = asyncHandler(async (req, res, next) => {
@@ -22,3 +23,73 @@ exports.product_detail = asyncHandler(async (req, res, next) => {
 		product: product,
 	})
 })
+
+// Display Product Create
+exports.product_create_get = asyncHandler(async (req, res, next) => {
+	// Get all categories
+	const allCategories = await Category.find().sort({ name: 1 }).exec()
+
+	res.render("product_form", {
+		title: "Create Product",
+		categories: allCategories,
+	})
+})
+
+// Handle product create on POST.
+exports.product_create_post = [
+	// Validate and sanitize the fields
+	body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+	body("description", "Description must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("price", "Must be a valid price").trim().toFloat().escape(),
+	body("stock", "Must be a valid stock amount")
+		.trim()
+		.toInt()
+		.escape()
+		.isLength({ min: 1 }),
+	body("img_url", "Must be a valid image url").trim().isURL().escape(),
+	body("category.*").escape(),
+
+	// Process request after validation and sanitization.
+	asyncHandler(async (req, res, next) => {
+		// Extract validation errors.
+		const errors = validationResult(req)
+
+		// Create product object with sanitized data.
+		const product = new Product({
+			name: req.body.name,
+			description: req.body.description,
+			price: req.body.price,
+			stock: req.body.stock,
+			img_url: req.body.img_url,
+			category: req.body.category,
+		})
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/error messages.
+
+			// Get all categories for form.
+			const allCategories = await Category.find().sort({ name: 1 }).exec()
+
+			// Mark selected category as checked
+			for (const category of allCategories) {
+				if (product.category.includes(category._id)) {
+					category.checked = "true"
+				}
+			}
+
+			res.render("product_form", {
+				title: "Create Product",
+				categories: allCategories,
+				product: product,
+				errors: errors.array(),
+			})
+		} else {
+			// Data from form is saved.
+			await product.save()
+			res.redirect(product.url)
+		}
+	}),
+]
--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,12 @@ const router = express.Router()
 // Require category controller
 const product_controller = require("../controllers/productController")
 
+// GET Product create page
+router.get("/create", product_controller.product_create_get)
+
+// POST Product create page
+router.post("/create", product_controller.product_create_post)
+
 // GET Category detail page
 router.get("/:id", product_controller.product_detail)
 
